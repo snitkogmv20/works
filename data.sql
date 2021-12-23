4a7e18b1d478c0dd64ae4142c0fee14bd7160d24
snitkogmv20 <95702202+snitkogmv20@users.noreply.github.com>
2021-12-23 15:31:01 +0300

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,16 @@
+insert into usr (first_name, middle_name, last_name, email) values
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com');
+
+insert into usr (first_name, middle_name, last_name, email) values
+('Ide', '1Иванович', '1Иванов', 'ivVFanov@my.example.com'),
+('FEFE', '2Петрович', '2Петров', 'petrfvdov@my.example.com'),
+('vSNF', '3Петрович', '3Сидоров', 'sidor23ov@my.example.com'),
+('FEFsd', '4Иванович', '4Иванов', 'ivanovwre@my.example.com'),
+('LIDER', '5Петрович', '5Петров', 'petrov45@my.example.com'),
+('AUT', '6Петрович', '6Сидоров', 'sidorov23@my.example.com'),
+('GIDEN', '7Иванович', '7Иванов', 'ivanovfd@my.example.com'),
+('FROM', '8Петрович', '8Петров', 'petrov43@my.example.com'),
+('GILZA', '9Петрович', '9Сидоров', 'sidorov76@my.example.com'),
+('fIKIER', '10Петрович', '10Сидоров', 'sidorov2ee@my.example.com');
