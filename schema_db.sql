06398c5514e082373b34ee264fc9b3b6dacabeca
snitkogmv20 <95702202+snitkogmv20@users.noreply.github.com>
2022-02-03 21:55:09 +0300

    Update schema_db.sql

--- schema_db.sql
+++ schema_db.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS public.usr
     id bigint NOT NULL DEFAULT nextval('usr_id_seq'::regclass),
     email character varying(64) COLLATE pg_catalog."default" NOT NULL,
     first_name character varying(64) COLLATE pg_catalog."default" NOT NULL,
-    last_name character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(64) COLLATE pg_catalog."default" NOT NULL
     middle_name character varying(64) COLLATE pg_catalog."default" NOT NULL
 )
 
